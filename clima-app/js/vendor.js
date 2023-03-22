63eaed8530f205110e81f5175e5c4760f803cb7a
Elan Deyan <74877042+ElanDeyan@users.noreply.github.com>
2023-03-22 14:31:14 -0300

    adding default to export and converting arrow function to function declaration in export default

--- clima-app/js/vendor.js
+++ clima-app/js/vendor.js
@@ -3,7 +3,7 @@
  * @param {string} time12h 
  * @returns Time formatted
  */
-export const convertTime12to24 = (time12h) => {
+export default function convertTime12to24(time12h) {
     const [time, modifier] = time12h.split(' ');
 
     let [hours, minutes] = time.split(':');
